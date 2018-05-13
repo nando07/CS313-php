483a74e5419edf8a8d20e1ae541e9e6450fac94b
Nando <redchameleonapps@gmail.com>
2018-05-13 02:20:34 -0500

    updates

--- web/shoppingCart/myCart.php
+++ web/shoppingCart/myCart.php
@@ -2,13 +2,7 @@
 // Start the session
 session_start();
 
-// Declaring session array
-// $_SESSION['pName'] = $_POST['name'];
-// $_SESSION['pImage'] = $_POST['image'];
-// // $_SESSION['pQuantity'] = $_POST['img'];
-// $_SESSION['pPrice'] = $_POST['price'];
-$_POST['img'];
-array_push($_SESSION['cart'], $_POST['item']; );
+array_push($_SESSION['cart'], $_POST['item']);
 
 echo "Number of Items in the cart = ".sizeof($_SESSION['cart'])." <a href=cart-remove-all.php>Remove all</a><br>";
 ?>

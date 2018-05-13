17bb90108bc0a057cc0ffb47cc97c856e46c8cc7
Nando <redchameleonapps@gmail.com>
2018-05-13 02:27:59 -0500

    updates

--- web/shoppingCart/myCart.php
+++ web/shoppingCart/myCart.php
@@ -5,7 +5,7 @@ session_start();
 array_push($_SESSION['cart'], $_POST['item']);
 
 echo "Number of Items in the cart = ".sizeof($_SESSION['cart'])." <a href=cart-remove-all.php>Remove all</a><br>";
-
-echo '<pre>'; print_r($_SESSION['cart']); echo '</pre>';
-}
+echo '<pre>'; 
+print_r($_SESSION['cart']); 
+echo '</pre>';
 ?>

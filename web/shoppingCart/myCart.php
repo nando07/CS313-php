ffde59a4aae19e8fbdc8b588c7df0c4be6a9325b
Nando <redchameleonapps@gmail.com>
2018-05-14 11:47:48 -0500

    updates

--- web/shoppingCart/myCart.php
+++ web/shoppingCart/myCart.php
@@ -40,8 +40,7 @@ session_start();
             echo '<div class="img-wrap">';
             echo '<img src="https://product2.djicdn.com/uploads/photos/33967/medium_a7f3e87a-e907-4eef-8755-4a704155b682.jpg" alt="mavicAir" /></div>';
             echo '<span>Mavic Air</span><strong>$399</strong>';
-            echo '<span>X  </span>';
-            echo $_SESSION["mavicQ"];
+            echo '<span>X  </span>' . $_SESSION["mavicQ"];
             echo '<div class="cart-item-border"></div>';
             echo '<div class="delete-item"></div></div>';
         }?>

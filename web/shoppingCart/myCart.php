46f6eae6d9cf13c2a4f8c10b2894d6d2d9681b82
Nando <redchameleonapps@gmail.com>
2018-05-14 17:45:40 -0500

    updates

--- web/shoppingCart/myCart.php
+++ web/shoppingCart/myCart.php
@@ -40,7 +40,6 @@ session_start();
             echo '<span>Mavic Air</span><strong>$399</strong>' . '  x  ' . $_SESSION["mavicQ"];
             echo '<div class="cart-item-border"></div>';
             echo '<a class="delete-item" href="deleteData.php?cartInfo=m"></a></div>';
-            // echo '<input type="button" value="delete" href="deleteData.php?cartInfo=m"/>';
         }
 
             if ($_SESSION["sparkQ"] > 0) {
@@ -51,7 +50,7 @@ session_start();
                 echo '<img src="https://product2.djicdn.com/uploads/photos/34841/medium_960b93a7-044f-49f6-b450-6d57b9afd829.jpg" alt="spark" /></div>';
                 echo '<span>Spark</span><strong>$399</strong>' . '  x  ' . $_SESSION["sparkQ"];
                 echo '<div class="cart-item-border"></div>';
-                echo '<div class="delete-item"></div></div>';
+                echo '<a class="delete-item" href="deleteData.php?cartInfo=s"></a></div>';
             }
 
                 if ($_SESSION["ph4Q"] > 0) {
@@ -62,7 +61,7 @@ session_start();
                     echo '<img src="https://product2.djicdn.com/uploads/photos/34734/medium_baada688-b74f-4bee-9a2d-9ff2e9782b7d.jpg" alt="ph4" /></div>';
                     echo '<span>Phantom 4</span><strong>$1,499</strong>' . '  x  ' . $_SESSION["ph4Q"];
                     echo '<div class="cart-item-border"></div>';
-                    echo '<div class="delete-item"></div></div>';
+                    echo '<a class="delete-item" href="deleteData.php?cartInfo=ph4"></a></div>';
                 }
 
                     if ($_SESSION["ph3Q"] > 0) {
@@ -73,7 +72,7 @@ session_start();
                         echo '<img src="https://product2.djicdn.com/uploads/photos/6711/medium_P3C_01.jpg" alt="ph3" /></div>';
                         echo '<span>Phantom 3</span><strong>$399</strong>' . '  x  ' . $_SESSION["ph3Q"];
                         echo '<div class="cart-item-border"></div>';
-                        echo '<div class="delete-item"></div></div>';
+                        echo '<a class="delete-item" href="deleteData.php?cartInfo=ph3"></a></div>';
                     }
 
                         if ($_SESSION["inspire2Q"] > 0) {
@@ -84,7 +83,7 @@ session_start();
                             echo '<img src="https://product1.djicdn.com/uploads/photos/405/medium_9351802d-9e88-4bb6-979c-48fe81748a33.png" alt="in2" /></div>';
                             echo '<span>Inspire 2</span><strong>$2,999</strong>' . '  x  ' . $_SESSION["inspire2Q"];
                             echo '<div class="cart-item-border"></div>';
-                            echo '<div class="delete-item"></div></div>';
+                            echo '<a class="delete-item" href="deleteData.php?cartInfo=in2"></a></div>';
                         }
 
                             if ($_SESSION["m600Q"] > 0) {
@@ -95,7 +94,7 @@ session_start();
                                 echo '<img src="https://product4.djicdn.com/uploads/photos/260/medium_f013bf4e-ba1d-4833-88b3-2d3ba62251dd.jpg" alt="m600" /></div>';
                                 echo '<span>Matrice 600</span><strong>$4,999</strong>' . '  x  ' . $_SESSION["m600Q"];
                                 echo '<div class="cart-item-border"></div>';
-                                echo '<div class="delete-item"></div></div>';
+                                echo '<a class="delete-item" href="deleteData.php?cartInfo=m600"></a></div>';
                             }
 
                                 if ($_SESSION["m100Q"] > 0) {
@@ -106,7 +105,7 @@ session_start();
                                     echo '<img src="https://product2.djicdn.com/uploads/photos/3521/medium_p1.jpg" alt="m100" /></div>';
                                     echo '<span>Matrice 100</span><strong>$3,299</strong>' . '  x  ' . $_SESSION["m100Q"];
                                     echo '<div class="cart-item-border"></div>';
-                                    echo '<div class="delete-item"></div></div>';
+                                    echo '<a class="delete-item" href="deleteData.php?cartInfo=m100"></a></div>';
                                 }
 
                                     if ($_SESSION["dGogglesQ"] > 0) {
@@ -117,7 +116,7 @@ session_start();
                                         echo '<img src="https://product1.djicdn.com/uploads/photos/33591/medium_64d4667d-431c-4285-9675-2dfe75f7404d.jpg" alt="dG" /></div>';
                                         echo '<span>DJI Goggles</span><strong>$549</strong>' . '  x  ' . $_SESSION["dGogglesQ"];
                                         echo '<div class="cart-item-border"></div>';
-                                        echo '<div class="delete-item"></div></div>';
+                                        echo '<a class="delete-item" href="deleteData.php?cartInfo=dg"></a></div>';
                                     }
         ?>
         </div>

36b29f9c89c086493872bc1dafa28a222ae9c8c4
Nando <redchameleonapps@gmail.com>
2018-06-03 20:11:48 -0500

    added updated files

--- web/Fitness-Tracker/insertCrunches.php
+++ web/Fitness-Tracker/insertCrunches.php
@@ -1,9 +1,14 @@
 <?php
+session_start();
 
 $standard = $_POST['abStandard'];
 $reverse = $_POST['abReverse'];
 $twisting = $_POST['abTwisting'];
 $tuck = $_POST['abTuck'];
+
+// $t=time();
+// $formattedTime = date()
+// echo(date("Y-m-d h:i:s",$t));
 
 require("connectToDB.php");
 $db = get_db();
@@ -20,6 +25,32 @@ $statement->bindValue(':twisting', $twisting);
 $statement->bindValue(':tuck', $tuck);
 
 $statement->execute();
+
+$username = $_SESSION['username_session'];
+$password = $_SESSION['password_session'];
+$statement = $db->prepare("SELECT id FROM workout.user 
+WHERE workout.user.username = '$username'
+AND workout.user.password = '$password'");
+
+$row = $statement->fetch(PDO::FETCH_ASSOC);
+
+$userId = $row['id'];
+$t = time();
+$formattedT = date("Y-m-d h:i:s",$t);
+
+$crunchesId = $db->lastInsertId("crunches_id_seq");
+
+$statement = $db->prepare('INSERT INTO workout.session(date, user_id, pushups_id, crunches_id) 
+VALUES (:ctime, :user, :pushupsID, :crunchesID)');
+
+$statement->bindValue(':ctime', $formattedT);
+$statement->bindValue(':user', $userId);
+$statement->bindValue(':pushupsID', null);
+$statement->bindValue(':crunchesID', $crunchesId);
+
+$statement->execute();
+
+
 
 } catch (Exception $ex) {
     echo "Error in database. Details: $ex";

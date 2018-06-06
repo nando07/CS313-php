10990cf72ecfd10423dda449c8c262cd17b8d146
Nando <redchameleonapps@gmail.com>
2018-06-05 20:37:22 -0500

    improved form to validate username and password

--- web/Fitness-Tracker/stats.php
+++ web/Fitness-Tracker/stats.php
@@ -27,8 +27,8 @@ session_start();
         <div class="container">
           <div class="row">
             <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
-              <p class="text-muted">This is a site that will help you track your fitness goals!</p>
+              <h4 class="text-white">Welcome</h4>
+              <p class="text-muted"><?php echo "Welcome " . $_SESSION['username_session']; ?></p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
               <h4 class="text-white">Contact</h4>
@@ -47,7 +47,7 @@ session_start();
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
             <strong>Go Back</strong>
           </a>
-		  <p class="navbar-brand d-flex align-items-center"><?php echo "Welcome " . $_SESSION['username_session']; ?> </p> 
+		   
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>

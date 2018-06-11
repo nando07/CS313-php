c1d15d3555ab19866734045fff44e477b40fce0f
Nando <redchameleonapps@gmail.com>
2018-06-11 01:06:14 -0500

    updates

--- web/Fitness-Tracker/edit_crunches.php
+++ web/Fitness-Tracker/edit_crunches.php
@@ -19,6 +19,8 @@ $id = $_REQUEST['id'];
 	<title>stats</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/bootstrap.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+
 	<link href="styles.css" rel="stylesheet">
 </head>
 <body>
@@ -27,15 +29,15 @@ $id = $_REQUEST['id'];
         <div class="container">
           <div class="row">
             <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
+              <h4 style="font-family: 'Permanent Marker', cursive;" class="text-white">About</h4>
               <p class="text-muted">This is a site that will help you track your fitness goals!</p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
+              <h4 style="font-family: 'Permanent Marker', cursive;" class="text-white">Contact</h4>
               <ul class="list-unstyled">
                 <li><a href="#" class="text-white">Follow on Twitter</a></li>
                 <li><a href="#" class="text-white">Like on Facebook</a></li>
-                <li><a href="#" class="text-white">Email me</a></li>
+                <li><a href="signOut.php" class="text-white">Sign Out</a></li>
               </ul>
             </div>
           </div>
@@ -45,7 +47,7 @@ $id = $_REQUEST['id'];
         <div class="container d-flex justify-content-between">
           <a href="stats.php" class="navbar-brand d-flex align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>Go to Stats</strong>
+            <strong style="font-family: 'Permanent Marker', cursive;">Go to Stats</strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- web/Fitness-Tracker/stats.php
+++ web/Fitness-Tracker/stats.php
@@ -27,11 +27,11 @@ if (!isset($_SESSION['username_session'])) {
         <div class="container">
           <div class="row">
             <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">Welcome</h4>
+              <h4 style="font-family: 'Permanent Marker', cursive;" class="text-white">Welcome</h4>
               <p class="text-muted"><?php echo $_SESSION['username_session']; ?></p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
+              <h4 style="font-family: 'Permanent Marker', cursive;" class="text-white">Contact</h4>
               <ul class="list-unstyled">
                 <li><a href="#" class="text-white">Follow on Twitter</a></li>
                 <li><a href="#" class="text-white">Like on Facebook</a></li>
